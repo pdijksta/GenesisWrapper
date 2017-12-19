257a69e9773ba759dbaeb47de4e62ec0f47a154e
Philipp Dijkstal <philipp.dijkstal@web.de>
2017-12-19 10:02:20 +0100

    added InputParser

--- simulation.py
+++ simulation.py
@@ -1,10 +1,13 @@
+import os
 import numpy as np
 import h5py
 
 class GenesisSimulation:
-    def __init__(self, filepath_in, filepath_out):
-        self.infile = filepath_in
-        self.outfile = filepath_out
+    def __init__(self, infile):
+        self.infile = infile
+        self.input = InputParser(self.infile)
+        dirname = os.path.dirname(self.infile)
+        self.outfile = os.path.join(dirname, self.input['rootname']+'.out.h5')
 
         self._dict = {}
         self.zplot = self['Global/zplot']
@@ -24,10 +27,43 @@ class GenesisSimulation:
     def print_tree(self):
         def name_and_size(key, ff):
             try:
-                print((key, ff[key].shape))
+                print((key, ff[key].shape, ff[key].dtype))
             except:
                 print(key)
 
         with h5py.File(self.outfile, 'r') as ff:
             ff.visit(lambda x: name_and_size(x, ff))
 
+class InputParser(dict):
+
+
+    def __init__(self, infile):
+        dict.__init__(self)
+        self.infile = infile
+
+        with open(self.infile, 'r') as f:
+            lines = f.readlines()
+
+        self.update({
+            'npart': 8192.,
+            'sample': 2.,
+            })
+        for line in lines:
+            line = line.strip()
+            if '=' in line:
+                attr = line.split('=')[0].strip()
+                value = line.split('=')[-1].strip()
+                try:
+                    value = float(value)
+                except:
+                    pass
+                self[attr] = value
+
+
+    def estimate_memory(self):
+        n_slices = self['slen'] / self['lambda0'] / self['sample']
+        memory_field = self['ngrid']**2*n_slices*16
+        memory_beam = self['npart']*n_slices*6*8
+        safety_factor = 1.5
+
+        return (memory_field+memory_beam)*safety_factor
